d6ae42a809c537caeeeaa8714f7fcd639cba29f5
Yessou Sami <yessou.sami@gmail.com>
2015-11-26 17:43:05 +0100

    Create showdb.php

---
+++ Wifi-Personal-Tracker/showdb.php
@@ -0,0 +1,17 @@
+ <?php 
+$db=mysql_connect("localhost","user","password") or die ("Can't connect to db");
+mysql_select_db("database") or die ("Can't find db");
+$sql= "SELECT * FROM WigleWifiDB";
+$ris=mysql_query($sql) or die("query failed");
+
+   while ($riga = mysql_fetch_array($ris)) 
+      {
+		 
+		  if($riga['MAC']=="WigleWifi-1.4"){}
+		  if($riga['Latitude']=="board=MSM8974"){}
+		  else{
+		  $bssid = substr($riga['MAC'], 0, 5);
+        echo ",['".$riga['MAC']."',".$riga['Latitude'].",".$riga['Longitude'].",2]";
+      
+	 }
+      }?>
