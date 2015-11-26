080902e8310c8e1ccc01fd88f60a2c4f44d50c3f
Yessou Sami <yessou.sami@gmail.com>
2015-11-26 17:45:44 +0100

    Update wifimap.php

--- Wifi-Personal-Tracker/wifimap.php
+++ Wifi-Personal-Tracker/wifimap.php
@@ -1,4 +1,7 @@
 
+<!--Wifi-Personal-Tracker Copyright (C) 2015 Sami Yessou
+This program comes with ABSOLUTELY NO WARRANTY;
+This is free software, and you are welcome to redistribute it under certain conditions -->
 <!DOCTYPE html>
 <html> 
 <head> 
